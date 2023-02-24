65cd7fae68cd2387ccfe3cf331824d9decba7e90
Bellmere <therise00@gmail.com>
2023-02-24 13:57:53 +0200

    Update UserMenu.js

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -7,12 +7,13 @@ export const UserMenu = () => {
     const dispatch = useDispatch();
     const { user } = useAuth();
 
-    const handleLogOut = () => dispatch(logOut());
 
     return (
         <div className={css.wrapper}>
-            <p className={css.username}>{user.name}</p>
-            <button className={css.logOut} type='button' onClick={handleLogOut}>Logout</button>
+            <p className={css.username}>Welcome, {user.name}</p>
+            <button type="button" onClick={() => dispatch(logOut())}>
+                Logout
+            </button>
         </div>
     );
 };
